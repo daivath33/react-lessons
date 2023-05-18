bf64970b7bf35c472c79d478d691298c86d4e496
daiva <daivath33@gmail.com>
2023-05-18 21:21:09 +0300

    added styles

--- 2_paskaita/react-app2/src/App.js
+++ 2_paskaita/react-app2/src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
 import Car from "./components/Car";
+import Counter from "./components/Counter";
 import FuncBestCar from "./components/FuncBestCar";
 import FuncCar from "./components/FuncCar";
 
 function App() {
   return (
-    <div className="flex-box">
-      <Car />
-      <FuncCar />
-      <FuncBestCar />
-    </div>
+    <>
+      <div className="flex-box">
+        <Car />
+        <FuncCar />
+        <FuncBestCar />
+      </div>
+      <h1>COUNTER</h1>
+      <div className="flex-box">
+        <Counter />
+      </div>
+    </>
   );
 }
 
---
+++ 2_paskaita/react-app2/src/components/Counter.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    setCount(count + 1);
+  };
+  const decrease = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className="counter">
+      <button onClick={increase}>+</button>
+      <h3>{count}</h3>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
