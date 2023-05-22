e2ff5c3495363171b1a79490142ff206fbea942b
daiva <daivath33@gmail.com>
2023-05-22 20:23:15 +0300

    added Button component

--- 3-react-app/src/App.js
+++ 3-react-app/src/App.js
@@ -1,11 +1,20 @@
 import "./App.css";
 import Hero from "./components/hero/Hero";
+import Button from "./components/button/Button";
 
 function App() {
   return (
-    <div>
-      <Hero title="Info Hero" subtitle="Info subtitle" />
-    </div>
+    <>
+      <div>
+        <Hero title="Info Hero" subtitle="Info subtitle" />
+      </div>
+      <div className="mg-t-30 flex-box">
+        <Button title="text" variant="text" />
+
+        <Button title="contained" variant="contained" />
+        <Button title="outlined" variant="outlined" />
+      </div>
+    </>
   );
 }
 
