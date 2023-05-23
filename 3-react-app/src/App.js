62f68918261db8c92b790a524e5a34cef651e402
daiva <daivath33@gmail.com>
2023-05-23 21:04:38 +0300

    created react-app

--- 3-react-app/src/App.js
+++ 3-react-app/src/App.js
@@ -4,6 +4,17 @@ import Button from "./components/button/Button";
 import MoodChecker from "./components/moodChecker/MoodChecker";
 import Product from "./components/product/Product";
 function App() {
+  const product = {
+    image1:
+      "https://cdn.shopify.com/s/files/1/0563/1400/3629/products/2024ExoticCars11.jpg?v=1676036099",
+    image2:
+      "https://cdn.shopify.com/s/files/1/0563/1400/3629/products/2024ExoticCars1.jpg?v=1676036099",
+    title: "Volkswagen GOLF MK2",
+    price: 10,
+    availability: 10,
+    description: "All pieces are printed on heavyweight 200 gsm art paper.",
+    category: "Calendars",
+  };
   return (
     <>
       <div>
@@ -20,7 +31,15 @@ function App() {
         <MoodChecker />
       </div>
       <hr />
-      <Product />
+      <Product
+        image1={product.image1}
+        image2={product.image2}
+        title={product.title}
+        price={product.price}
+        availability={product.availability}
+        description={product.description}
+        category={product.category}
+      />
     </>
   );
 }
--- 3-react-app/src/components/product/Product.jsx
+++ 3-react-app/src/components/product/Product.jsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
 
 import "./Product.css";
-const Product = () => {
+const Product = ({
+  image1,
+  image2,
+  title,
+  price,
+  description,
+  availability,
+  category,
+}) => {
+  const [count, setCount] = useState(0);
+  const increase = () => {
+    if (count < availability) setCount(count + 1);
+  };
+  const decrease = () => {
+    if (count > 0) setCount(count - 1);
+  };
   return (
     <div className="card">
       <div className="left">
-        <img
-          src="https://cdn.shopify.com/s/files/1/0563/1400/3629/products/2024ExoticCars11.jpg?v=1676036099"
-          alt=""
-        />
-        <img
-          src="https://cdn.shopify.com/s/files/1/0563/1400/3629/products/2024ExoticCars1.jpg?v=1676036099"
-          alt=""
-        />
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
       </div>
       <div className="right">
         <div className="card-heading ">
           <div>
-            <h3>Volkswagen GOLF MK2</h3>
+            <h3>{title}</h3>
             <h3>2022 Calendar</h3>
           </div>
           <div className="arrow-icons">
@@ -31,22 +40,26 @@ const Product = () => {
           </div>
         </div>
 
-        <p className="price">10.00€</p>
+        <p className="price">{price.toFixed(2)}€</p>
         <p>12 unique designs</p>
-        <p>All pieces are printed on heavyweight 200 gsm art paper.</p>
-        <p>Limited to 50pieces</p>
+        <p>{description}</p>
+        <p>Limited to 50 pieces</p>
         <p>One size:</p>
-        <p>11.7x16.5 inch/(297x20mm)</p>
+        <p>11.7x16.5 inch/(297x420mm)</p>
         <h4>
-          Availability: <span>10 in stock</span>
+          Availability: <span>{availability} in stock</span>
         </h4>
         <div className="btn-group">
-          <button className="btn-count">+</button>
-          <p className="count">0</p>
-          <button className="btn-count">-</button>
+          <button className="btn-count" onClick={decrease}>
+            -
+          </button>
+          <p className="count">{count}</p>
+          <button className="btn-count" onClick={increase}>
+            +
+          </button>
           <button className="btncal">ADD TO CART</button>
         </div>
-        <p>Category: Calendars</p>
+        <p className="card-footer">Category: {category}</p>
       </div>
     </div>
   );
